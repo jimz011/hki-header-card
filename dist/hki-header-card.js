853b297b58fed276a59a116927073e8248caeb6a
Jimmy Schings <47046878+jimz011@users.noreply.github.com>
2026-01-20 13:29:28 +0100

    Update hki-header-card.js

--- dist/hki-header-card.js
+++ dist/hki-header-card.js
@@ -5,7 +5,7 @@ import { LitElement, html, css } from "https://unpkg.com/lit@2.8.0/index.js?modu
 const CARD_NAME = "hki-header-card";
 
 console.info(
-  '%c HKI-HEADER-CARD %c v1.3.3 ',
+  '%c HKI-HEADER-CARD %c v1.3.4 ',
   'color: white; background: #17a2b8; font-weight: bold;',
   'color: #17a2b8; background: white; font-weight: bold;'
 );
